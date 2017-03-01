2782bb2a6adcd2c62cd5136f0108a95ee5d154a1
nkbelow <nkbelow@gmail.com>
2017-03-01 15:57:13 -0800

    Able to serve index.html

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var fs = require('fs');
+var http = require('http');
 var archive = require('../helpers/archive-helpers');
 
 exports.headers = {
@@ -14,8 +15,25 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  fs.readFile(asset, 'utf-8', function (err, html) {
+    if (err) { throw err; }
+    res.write(html);
+    res.end();
+    console.log('Sent!');
+  });
 };
 
+exports.buildResponse = function(response, asset, callback, statusCode) {
 
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, exports.headers);
+
+  if (asset) {
+    exports.serveAssets(response, asset, callback);
+  } else {
+    response.end();
+  }
+
+};
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,37 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('./http-helpers');
 // require more modules/folders here!
 
+var paths = {
+  '/': archive.paths.siteAssets + '/index.html',
+  '/styles.css': archive.paths.siteAssets + '/styles.css',
+  '/favicon.ico': null,
+  '/loading.html': archive.paths.siteAssets + '/loading.html'
+};
+
+var actions = {
+  'GET': function (request, response) {
+    // send a response back
+    if (paths[request.url]) {
+      httpHelper.buildResponse(response, paths[request.url]); 
+    }
+  },
+  'POST': function(request, response) {
+    // Add data to server
+    // And send response back
+  },
+  'OPTIONS': function(request, response) {
+    // sends an A-OK response back
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('Request:', req.method, 'URL:', req.url);
+  if (actions[req.method]) {
+    actions[req.method](req, res);
+  } else {
+    // send response with 404 status
+    httpHelper.buildResponse(res, function() {}, 404);
+  }
 };
